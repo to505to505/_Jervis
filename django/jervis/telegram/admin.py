2f0de98e611a9d86a11770d6a776deb2565c779e
kwazzart <vladimirkwaz@gmail.com>
2023-04-15 00:39:22 +0300

    Base model created
    Image and Chat models added

--- django/jervis/telegram/admin.py
+++ django/jervis/telegram/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+class ImageAdmin(admin.ModelAdmin):
+    pass
+
+class ChatAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Chat, ChatAdmin)
+admin.site.register(Image, ImageAdmin)
--- django/jervis/telegram/models.py
+++ django/jervis/telegram/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Chat(models.Model):
+    chat_id = models.IntegerField(unique=True, null=True)
+    generation_amount = models.IntegerField(default=5)
+
+    def __str__(self) -> str:
+        return f"tg_id: {self.tg_channel_id}"
+    
+class Image(models.Model):
+    chat = models.ForeignKey(Chat, null=True, on_delete=models.SET_NULL)
+    image_url = models.URLField()
+    messageid_sseed = models.CharField(max_length=70, unique=True)
+    creation_time = models.DateTimeField(auto_now_add=True)
