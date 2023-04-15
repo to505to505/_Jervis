17b5dfbb6bd7299e5334a2d073479d9ccfccebbf
pavelsL2SH <pavels1279@yandex.ru>
2023-04-15 10:56:34 +0300

    devided to api and Bot programs and they are working well

--- discord/app/jervisDiscordBot.py
+++ discord/app/jervisDiscordBot.py
@@ -19,24 +19,24 @@ auth = {
 async def root():
     return {"message": "Hello World"}
 
-#@app.post("current_prompt")
-#async def current_prompt(input_text):
-#    msg = {"type":2,
-#    "application_id":"1032699319368814652",
-#    "guild_id":"1094995526769987704",
-#    "channel_id":"1095343240594595900",
-#    "session_id":"2e224721645708c191a4f38585994b53",
-#    "data":
-#    {"version":"1093695818164338708",
-#    "id":"1037145598782099518",
-#    "name":"generate",
-#    "type":1,
-#    "options":[{"type":3,
-#    "name":"description",
-#    "value":input_text}],
-#    }}
-#    requests.post(url, headers = auth, json = msg)
-#    print(f"sent a /generate command with prompt: {input_text}")
+@app.post("current_prompt")
+async def current_prompt(input_text):
+    msg = {"type":2,
+    "application_id":"1032699319368814652",
+    "guild_id":"1094995526769987704",
+    "channel_id":"1095343240594595900",
+    "session_id":"2e224721645708c191a4f38585994b53",
+    "data":
+    {"version":"1093695818164338708",
+    "id":"1037145598782099518",
+    "name":"generate",
+    "type":1,
+    "options":[{"type":3,
+    "name":"description",
+    "value":input_text}],
+    }}
+    requests.post(url, headers = auth, json = msg)
+    print(f"sent a /generate command with prompt: {input_text}")
 
 @bot.event
 async def on_ready():
