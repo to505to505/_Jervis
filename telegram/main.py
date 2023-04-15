e47074af5da4eb815e9d551f96104cfa824250a3
to505to505 <sahappleid@yandex.ru>
2023-04-15 10:51:14 +0300

    upgrade

--- telegram/main.py
+++ telegram/main.py
@@ -3,6 +3,9 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.utils import executor
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.types import Message
+from aiogram.dispatcher.filters import Text
+from aiogram.types import InputFile
+import time
 
 
 logging.basicConfig(level=logging.INFO)
@@ -28,50 +31,82 @@ button3 = KeyboardButton("Техническая поддержка")
 keyboard.row(button1, button2)
 keyboard.add(button3)
 
-# Define the message handler function
-@dp.message_handler(text=["Сгенерировать изображение", "Мой профиль", "Техническая поддержка"])
-async def handle_keyboard(message: Message):
-    chat_id = message.chat.id
-    if message.text == "Сгенерировать изображение":
-        await bot.send_message(chat_id, 'Напишите prompt и прие', reply_markup = keyboard)
 
-        # Do something when Option 1 is selected  
-    elif message.text == "Мой профиль":
-        # Do something when Option 2 is selected
-        pass
-    elif message.text == "Техническая поддержка":
-        # Do something when Option 3 is selected
-        pass
+
+async def send_prompt(chat_id, prompt):
+    pass
+async def send_prompt_photo(chat_id, prompt, photo):
+    pass
+async def push_button(chat_id, propmpt, button):
+    pass
+
+async def send_result(chat_id, prompt, result):
+    with open('photo.jpg', 'rb') as photo:
+        await bot.send_photo(chat_id, photo=InputFile(photo))
+
+    button1 = InlineKeyboardButton(text="U1", callback_data="U1")
+    button2 = InlineKeyboardButton(text="U2", callback_data="U2")
+    button3 = InlineKeyboardButton(text="U3", callback_data="U3")
+    button4 = InlineKeyboardButton(text="U4", callback_data="U4")
+    button5 = InlineKeyboardButton(text="V1", callback_data="V1")
+    button6 = InlineKeyboardButton(text="V2", callback_data="V2")
+    button7 = InlineKeyboardButton(text="V3", callback_data="V3")
+    button8 = InlineKeyboardButton(text="V4", callback_data="V4")
+    button9 = InlineKeyboardButton(text="Reroll", callback_data="Reroll")
+
+    keyboard1 = InlineKeyboardMarkup()
+    keyboard1.row(button1, button2, button3, button4, button9)
+    keyboard1.row(button5, button6, button7, button8)
+
+
 
 @dp.message_handler(text="Сгенерировать изображение")
-async def handle_option_one(message: types.Message):
-    chat_id = message.chat.id
-    # Send a message to request the image
-    await bot.send_message(chat_id, 'Напишите prompt и прие')
+async def handle_generate(message: types.Message):
 
-    # Register a new handler to receive the image
+    chat_id = message.chat.id
+    await bot.send_message(chat_id, 'Напишите prompt и по желанию прикрепите картинку-референс.')
+
+    #только промпт
+    @dp.message_handler(content_types=types.ContentTypes.TEXT)
+    async def handle_prompt(message: types.Message):
+        prompt = message.text
+        await bot.send_message(chat_id, f'Изображение генерируется по запросу: \n{prompt}\n Пожалуйста, подождите!')
+        result = send_prompt(chat_id, prompt)
+    
+        send_result(chat_id, prompt, result)
+
+        @dp.message_handler(text="U1")
+        async def handle_U1(message: types.Message):
+            pass
+
+        
+    
+    #промпт + референс
     @dp.message_handler(content_types=types.ContentTypes.PHOTO)
-    async def handle_image(message: Message):
-    # Check if the message contains a caption
+    async def handle_photo(message: types.Message):
         if message.caption:
             caption = message.caption
         else:
-            caption = "No caption provided"
+            await bot.send_message(chat_id, 'Вы обязательно должны прикрепить prompt в сообщении с картинкой-референсом! Попробуйте еще раз.')
+
+        file_id = message.photo[-1].file_id
+        file = await bot.download_file_by_id(file_id)
+        with open("photo.jpg", "wb") as f:
+            f.write(file.read())
+        await bot.send_message(chat_id, f'Изображение генерируется по картинке-референсу и по запросу: \n{prompt}\n Пожалуйста, подождите!')
+        result = send_prompt_photo(chat_id, caption, file)
+        send_result(chat_id, caption, result)
+
     
-    # Get the largest image from the list of photo sizes
-    photo: PhotoSize = message.photo[-1]
-    chat_id = message.chat.id
-    
-    # Download the image and save it to a file
-    from aiogram.types import Message
-
-# Assuming 'message' is the Message object received from the user
-file_id = message.document.file_id  # or use message.photo.file_id for a photo
-file = await bot.get_file(file_id)
-filename = file.file_path.split("/")[-1]
-await file.download(destination=f"./folder/{filename}")
 
 
+
+
+
+
+
+# dp.register_message_handler(handle_photo, Text(equals="Option 1"), content_types=types.ContentTypes.PHOTO)
+#dp.register_message_handler(start_handler, commands=['start'])
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=False)
