3a60481079140ff39fc057869b32dcce1ca277e6
pavelsL2SH <pavels1279@yandex.ru>
2023-04-20 00:21:29 +0300

    file delete add(myabe dont work)))))

--- discord/app/discordBot.py
+++ discord/app/discordBot.py
@@ -94,14 +94,14 @@ async def on_message(message):
 
             messageid_sseed = f"{message.id}_{parce_get_sseed(file_drive_url)}"
 
-            send_image(messageid_sseed, file_drive_url, message.content)
+            await send_image(messageid_sseed, file_drive_url, message.content)
 
             logging.info(file_drive_url)
             logging.info(FILE_PATH)
             logging.info(f"File ID: {file_id}")
 
             # Удаляем локальную картинку
-            # os.remove(FILE_PATH)
+            os.remove(FILE_PATH)
 
         else:
             logging.warning("There are no attachment...")
