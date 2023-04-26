4110513c374343bc1c09e8c96263a47685ef86c2
kwazzart <vladimirkwaz@gmail.com>
2023-04-26 16:59:59 +0300

    Connecting midjourney #1

--- discord/app/api.py
+++ discord/app/api.py
@@ -8,6 +8,16 @@ import disnake
 from disnake.ext import commands
 from uvicorn import Config, Server
 from JervisRequests import *
+from pydantic import BaseModel
+import json
+
+class ImageGenerationRequest(BaseModel):
+    prompt: str
+
+class PushButtonRequest(BaseModel):
+    method: str
+    image_number: int
+    messageid_sseed: str
 
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
 
@@ -18,7 +28,11 @@ async def root():
     return {"message": "Hello World"}
 
 @app.post("/generate_image/")
-async def generate_image(input_text):
+async def generate_image(request: ImageGenerationRequest):
+    data = request.json()
+    data = json.loads(data)
+    input_text = data.get("prompt")
+    
     logging.info(f"sent a /generate_image command with prompt: {input_text}")
 
     get_image(input_text)
