05f198c27a3e686d03d03f82e269b0735a186ba8
to505to505 <sahappleid@yandex.ru>
2023-04-30 23:10:29 +0300

    changed buttons (U1, U2) callback_data

--- telegram/app/app_api.py
+++ telegram/app/app_api.py
@@ -82,15 +82,15 @@ async def download_photo(chat_id, tg_message_id, image_url):
 
 async def make_buttons():
     ''''Creating buttons as U1, U2, etc,'''
-    button1 = InlineKeyboardButton(text="U1", callback_data="Upscaled:1")
-    button2 = InlineKeyboardButton(text="U2", callback_data="Upscaled:2")
-    button3 = InlineKeyboardButton(text="U3", callback_data="Upscaled:3")
-    button4 = InlineKeyboardButton(text="U4", callback_data="Upscaled:4")
-    button5 = InlineKeyboardButton(text="V1", callback_data="Variations:1")
-    button6 = InlineKeyboardButton(text="V2", callback_data="Variations:2")
-    button7 = InlineKeyboardButton(text="V3", callback_data="Variations:3")
-    button8 = InlineKeyboardButton(text="V4", callback_data="Variations:4")
-    button9 = InlineKeyboardButton(text="Reroll", callback_data="Reroll:0")
+    button1 = InlineKeyboardButton(text="U1", callback_data="upsample:1")
+    button2 = InlineKeyboardButton(text="U2", callback_data="upsample:2")
+    button3 = InlineKeyboardButton(text="U3", callback_data="upsample:3")
+    button4 = InlineKeyboardButton(text="U4", callback_data="upsample:4")
+    button5 = InlineKeyboardButton(text="V1", callback_data="variation:1")
+    button6 = InlineKeyboardButton(text="V2", callback_data="variation:2")
+    button7 = InlineKeyboardButton(text="V3", callback_data="variation:3")
+    button8 = InlineKeyboardButton(text="V4", callback_data="variation:4")
+    button9 = InlineKeyboardButton(text="Reroll", callback_data="reroll:0")
 
     keyboard = InlineKeyboardMarkup()
     keyboard.row(button1, button2, button3, button4, button9)
